7f01a567dce863cadcf31f5864cbea59834ca281
Jared Spencer <jklz@me.com>
2024-10-01 20:59:41 -0600

    start setting up tests

---
+++ tests/Services/ValidationServiceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppTests\Services;
+
+use App\Services\ValidationService;
+use PHPUnit\Framework\TestCase;
+
+class ValidationServiceTest extends
+{
+    use CreatesApplication;
+
+    public function testValidate()
+    {
+
+    }
+
+    public function testGetValidatedValues()
+    {
+
+    }
+
+    public function testGetSafeValue()
+    {
+
+    }
+
+    public function testAddFieldConfig()
+    {
+
+    }
+
+    public function testIsValid()
+    {
+
+    }
+}
